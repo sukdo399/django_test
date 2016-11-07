f9fbae642cecf9da0ad32f976dbb462625dbf3e0
Seokha Ko <sh.ko@lge.com>
2016-11-07 00:27:10 -0500

    add remove button

--- data_app/views.py
+++ data_app/views.py
@@ -42,3 +42,11 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'data_app/post_edit.html', {'form': form})
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    # if request.method == "POST":
+    post.delete()
+
+    return redirect('post_list')
