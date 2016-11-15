8dedbc57271693bbd349bce8c6973223877d6922
Seokha Ko <sh.ko@lge.com>
2016-11-15 03:27:44 -0500

    add webkitdirectory for input tag.

--- data_app/forms.py
+++ data_app/forms.py
@@ -4,6 +4,9 @@ from .models import Post
 
 
 class PostForm(forms.ModelForm):
+    file = forms.FileField(widget=forms.ClearableFileInput(
+        attrs={'multiple': True, 'required': True, 'webkitdirectory': True, 'mozdirectory': True, 'msdirectory': True,
+               'odirectory': True, 'directory': True, "onclick": "uploadFilteredFiles()"}))
 
     class Meta:
         model = Post
